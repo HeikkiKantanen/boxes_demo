f4bdab028cc3d7793b00ea4c782efdb2ebbf0875
BC-macair015 <bc-macair015@BC-macair015s-MacBook-Air.local>
2021-10-26 13:16:30 +0300

    mapping

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import MainC from "./components/MainC";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainC />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import Box from "./Box";
+
+const Main = () => {
+  return (
+    <main>
+      <Box name="Maria" title="CEO" age="29" />
+      <Box name="Jen" title="Developper" age="25" />
+      <Box name="James" title="Designer" age="28" />
+    </main>
+  );
+};
+
+export default Main;
