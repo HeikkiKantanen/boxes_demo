93eeff504fe5984f70062358993c5e994140e854
BC-macair015 <bc-macair015@BC-macair015s-MacBook-Air.local>
2021-11-01 11:58:53 +0200

    added links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
-import MainC from "./components/MainC";
+// import MainC from "./components/MainC";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <MainC />
+      {/* <MainC /> */}
+      <Main />
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,45 @@
 import Box from "./Box";
+import { NavLink, Switch, Route } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is the HOME page</h1>
+    </div>
+  )
+}
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is the About page</h1>
+    </div>
+  )
+}
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>This is the Contact page</h1>
+    </div>
+  )
+}
 
 const Main = () => {
   return (
     <main>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="About">About</NavLink></li>
+          <li><NavLink to="/Contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+      </Switch>
       <Box name="Maria" title="CEO" age="29" />
       <Box name="Jen" title="Developper" age="25" />
       <Box name="James" title="Designer" age="28" />
